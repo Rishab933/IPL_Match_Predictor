09d0798982ae86559c545d9a8251ce74c11b1493
Rishab Jaiswal <jaiswalrishab933@gmail.com>
2025-02-22 23:07:21 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+teams = ['Chennai Super Kings',
+ 'Royal Challengers Bengaluru',
+ 'Rajasthan Royals',
+ 'Punjab Kings',
+ 'Kolkata Knight Riders',
+ 'Mumbai Indians',
+ 'Gujarat Titans',
+ 'Delhi Capitals',
+ 'Sunrisers Hyderabad',
+ 'Lucknow Super Giants']
+
+cities = ['Bengaluru', 'Chandigarh', 'Delhi', 'Mumbai', 'Kolkata', 'Jaipur',
+       'Hyderabad', 'Chennai', 'Cape Town', 'Port Elizabeth', 'Durban',
+       'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+       'Bloemfontein', 'Ahmedabad', 'Bhubaneswar', 'Nagpur', 'Dharamsala',
+       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+       'Indore', 'Dubai', 'Sharjah', 'Navi Mumbai', 'Lucknow', 'Guwahati',
+       'Mohali']
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+
+st.title('IPL Match Predictor')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select the batting team',teams)
+with col2:
+    bowling_team = st.selectbox('Select the bowling team',teams)
+
+selected_city = st.selectbox('Select the host city',sorted(cities))
+
+target = st.number_input('Target')
+
+col3, col4, col5 = st.columns(3)
+with col3:
+    score = st.number_input('Score')
+with col4:
+    overs = st.number_input('Over Completed')
+with col5:
+    wickets = st.number_input('Wickets Taken')
+
+if st.button('Predict Probability'):
+    runs_left = target - score
+    balls_left = 120 - (overs*6)
+    wickets = 10 - wickets
+    crr = score/overs
+    rrr = (runs_left*6)/balls_left
+
+    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],
+                             'city':[selected_city],'runs_left':[runs_left],
+                             'balls_left':[balls_left],'wickets_left':[wickets],
+                             'target':[target],'crr':[crr],'rrr':[rrr]})
+    #st.table(input_df)
+
+    result = pipe.predict_proba(input_df)
+    loss = result[0][0]
+    win = result[0][1]
+    col6, col7 = st.columns(2)
+    with col6:
+        st.subheader(batting_team + " - " + str(round(win * 100)) + "%")
+    with col7:
+        st.subheader(bowling_team + " - " + str(round(loss * 100)) + "%")
